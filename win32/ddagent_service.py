3851fdec4b02d2dc1d7aa76fafc5ed15aa1aaaaa
Etienne LAFARGE <etienne.lafarge@datadoghq.com>
2015-09-08 00:58:49 -0400

    Fixed wrong agent path in windows service

--- win32/ddagent_service.py
+++ win32/ddagent_service.py
@@ -67,7 +67,7 @@ class AgentService(win32serviceutil.ServiceFramework):
         self.log_path = os.path.join(_windows_commondata_path(), 'Datadog', 'logs', 'service.log')
 
         # Are we in a py2exed package or in a source install script or just a git pulled repo ?
-        if os.path.isfile(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
+        if not os.path.isfile(os.path.dirname(os.path.dirname(os.path.realpath(__file__))) + \
             "windows_supervisor.py"):
             self.agent_path = os.path.dirname(os.path.dirname(
                 os.path.dirname(os.path.dirname(os.path.realpath(__file__))))) + "\\agent"
